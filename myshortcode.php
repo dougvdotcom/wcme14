f1d69c12e36ea60f8bef9e1c9af4b3f001f8ab4d
Doug Vanderweide <dvanderweide@mainetoday.com>
2014-08-13 15:50:03 -0400

    initial commit

---
+++ myshortcode.php
@@ -0,0 +1,28 @@
+<?php
+/*
+* Plugin Name: My Shortcode Plugin
+* Plugin URI: http://www.example.com/path-to-some-page-that-this-plugin-calls-home.html
+* Description: This is my very first plugin, and it creates a shortcode that spits out a bunch of boilerplate text.
+* Version: 1.0
+* Author: Your Name Here
+* Author URI: http://mywebsite.com
+* License: GPL2
+*/
+
+function my_shortcode_plugin_callback($atts, $content = null) {
+	switch($atts['id']) {
+		case "1":
+			$msg = "Copyright &copy; " . date('Y') . " All rights reserved.";
+			break;
+		case "2":
+			$msg = "Like this post? Consider making a Paypal donation.";
+			break;
+		default:
+			$msg = '"Good questions outrank easy answers." &mdash; Paul Samuelson';
+			break;
+	}
+	return $msg;
+}
+
+add_shortcode('myshortcode', 'my_shortcode_plugin_callback');
+?>
